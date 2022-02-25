376488af83e15a3cc2c6373319e6d101444ccdd9
Cezar <cezarlantosgmail.com>
2022-02-25 17:10:31 +0100

    stamp movies

--- index.php
+++ index.php
@@ -16,18 +16,38 @@ class Movie{
     }
 
     public function getMovie() {
-        return 
-        $this->movieTitle . "<br/>";
-        $this->movieDescription . "<br/>";
-        $this->movieDate . "<br/>";
-        $this->movieGenre . "<br/>";
-        $this->movieLanguage . "<br/>";
+        return
+            "<div><h3>Title: " . $this->movieTitle . "</h3>" . 
+            "<p>Description: " . $this->movieDescription . "</p>" . 
+            "<p>Date: " . $this->movieDate . "</p>" .
+            "<p>Genre: " . $this->movieGenre . "</p>" .
+            "<p>Language: " . $this->movieLanguage . "</p>" . "</div>";
     }
 
-    $movieOne = new Movie("Peaky Blinders", "Birmingham mafia family", "27-02-2022", "Historical Fiction", "Eng");       
-    $movieTwo = new Movie("Legend", "Gangstar twins", "03-03-2016", "Drama", "Eng");       
 }
 
-var_dump($movieOne, $movieTwo)
+$movieOne = new Movie("Peaky Blinders", "Birmingham mafia family", "27-02-2022", "Historical Fiction", "Eng");       
+$movieTwo = new Movie("Legend", "Gangstar twins", "03-03-2016", "Drama", "Eng");       
+
+// var_dump($movieOne, $movieTwo)
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php  
+    echo ($movieOne -> getMovie());
+    echo ($movieTwo -> getMovie());
+?>
+
+
+</body>
+</html>
